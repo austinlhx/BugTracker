00f2da24474ebda8e114c835f7d7648a014e21f6
Duy Tran <duyvutran2303@gmail.com>
2021-11-21 12:17:39 -0500

    add login page + removing unnecessary icons in public

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
+import Login from './login'
 
 export default function Home() {
   return (
     <>
+      <Login />
     </>
   )
 }
---
+++ frontend/pages/login/index.js
@@ -0,0 +1,58 @@
+import {
+    Flex,
+    Box,
+    FormControl,
+    FormLabel,
+    Input,
+    Checkbox,
+    Stack,
+    Link,
+    Button,
+    Heading,
+    Text,
+    useColorModeValue,
+    Radio,
+    RadioGroup
+} from '@chakra-ui/react';
+
+export default function Login() {
+    return (
+        <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            bg={useColorModeValue('gray.50')}>
+            <Stack
+                spacing={4}
+                w={'full'}
+                maxW={'sm'}
+                bg={useColorModeValue('white', 'gray.700')}
+                rounded={'xl'}
+                boxShadow={'lg'}
+                py={6}
+                my={12}
+                align={'center'}>
+                <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+                    Login
+                </Heading>
+                <RadioGroup>
+                    <Stack direction="row">
+                        <Radio value="dev">Developer</Radio>
+                        <Radio value="admin">Admin</Radio>
+                    </Stack>
+                </RadioGroup>
+                <Stack spacing={6}>
+                    <Button
+                        size={'lg'}
+                        bg={'blue.400'}
+                        color={'white'}
+                        _hover={{
+                            bg: 'blue.500',
+                        }}>
+                        Login using Discord
+                    </Button>
+                </Stack>
+            </Stack>
+        </Flex>
+    )
+}
